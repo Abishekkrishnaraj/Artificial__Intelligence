efe8f6f3fdb8b661927b9871f6fa335fbfbb85aa
Abishekkrishnaraj <122676775+Abishekkrishnaraj@users.noreply.github.com>
2023-09-29 09:45:31 +0530

    Create setoperation.py

---
+++ setoperation.py
@@ -0,0 +1,8 @@
+
+# Artificial__Intelligence
+a={12,4,5,6,8,10}
+b={12,4,90,32,8,10}
+print("Union of a&b ;" , a|b)
+print("Intersection of a&b :",a&b)
+print("Difference of a&b :", a-b)
+print("Symmetric difference of a,b:",a^b)

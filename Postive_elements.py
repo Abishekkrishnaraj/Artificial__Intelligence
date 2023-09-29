868452a659cef8568e956b30fe7963f64a5d1dfc
Abishekkrishnaraj <122676775+Abishekkrishnaraj@users.noreply.github.com>
2023-09-29 09:35:53 +0530

    Create Postive_elements.py

---
+++ Postive_elements.py
@@ -0,0 +1,12 @@
+# Artificial__Intelligence
+l=[]
+k=[]
+n=int(input("no.ofelements"))
+for i in range(0,n):
+  m=int(input("the element"))
+  l.append(m)
+for i in l:
+  if(i>o):
+    k.append(i)
+print(k)
+    
